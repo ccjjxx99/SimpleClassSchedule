62a886fa0fae33e66445887ccf726acf2bad089e
theoryxsy <2017302580167@whu.edu.com>
2018-12-07 11:45:12 +0800

    xsy

--- SimpleClassSchedule/SimpleClassSchedule/MainActivity.cs
+++ SimpleClassSchedule/SimpleClassSchedule/MainActivity.cs
@@ -45,7 +45,7 @@ namespace SimpleClassSchedule
         private void FabOnClick(object sender, EventArgs eventArgs)
         {
             View view = (View) sender;
-            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
+            Snackbar.Make(view, "Hello", Snackbar.LengthLong)
                 .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
         }
 	}
