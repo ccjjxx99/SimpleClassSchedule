ef8b799e9f12c8f461d5ea411ea154d092d25cff
常佳鑫 <ccjjxx99@qq.com>
2018-12-22 14:19:43 +0800

    改为中文名，禁止横屏

--- SimpleClassSchedule/SimpleClassSchedule/SimpleClassSchedule.Android/MainActivity.cs
+++ SimpleClassSchedule/SimpleClassSchedule/SimpleClassSchedule.Android/MainActivity.cs
@@ -9,13 +9,15 @@ using Android.OS;
 
 namespace SimpleClassSchedule.Droid
 {
-    [Activity(Label = "SimpleClassSchedule", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "简约课程表", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
+
+            this.RequestedOrientation = Android.Content.PM.ScreenOrientation.Portrait;//竖屏，禁止横屏
 
             base.OnCreate(savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
